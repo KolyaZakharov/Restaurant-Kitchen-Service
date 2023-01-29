599c45169bb0d351ea59189cbd008082a72310b4
Mykola Zakharov <kolyazakharov94@gmail.com>
2023-01-29 14:57:21 +0200

    used black for project and app

--- restaurant_kitchen/apps.py
+++ restaurant_kitchen/apps.py
@@ -2,5 +2,5 @@ from django.apps import AppConfig
 
 
 class RestaurantKichenConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'restaurant_kichen'
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "restaurant_kichen"
