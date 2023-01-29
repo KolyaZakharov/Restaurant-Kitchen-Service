7b58dc787bc849136f086ac41bcf204e166ec259
Mykola Zakharov <kolyazakharov94@gmail.com>
2023-01-29 21:31:07 +0200

    created 4 models, overloading __str__ and added AUTH_USER_MODEL in settings

--- restaurant_kitchen/models.py
+++ restaurant_kitchen/models.py
@@ -1,3 +1,49 @@
+from django.conf import settings
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+
+class DishType(models.Model):
+    name = models.CharField(max_length=255)
+
+    class Meta:
+        ordering = ["name"]
+
+    def __str__(self):
+        return self.name
+
+
+class Ingredient(models.Model):
+    name = models.CharField(max_length=255)
+
+    class Meta:
+        ordering = ["name"]
+
+    def __str__(self):
+        return self.name
+
+
+class Cook(AbstractUser):
+    years_of_experience = models.IntegerField(null=True)
+
+    def __str__(self):
+        return f"{self.username} ({self.first_name} {self.last_name})"
+
+    class Meta:
+        ordering = ["username"]
+
+
+class Dish(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=7, decimal_places=2)
+    dish_type = models.ForeignKey(DishType, on_delete=models.CASCADE)
+    cooks = models.ManyToManyField(settings.AUTH_USER_MODEL, related_name="dishes")
+    ingredients = models.ManyToManyField(Ingredient, related_name="dishes")
+
+    class Meta:
+        ordering = ["name"]
+
+    def __str__(self):
+        return f"{self.name} (price:{self.price}, dish type:{self.dish_type.name}"
+
--- restaurant_kitchen_service/settings.py
+++ restaurant_kitchen_service/settings.py
@@ -99,6 +99,7 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+AUTH_USER_MODEL = "restaurant_kitchen.Cook"
 
 # Internationalization
 # https://docs.djangoproject.com/en/4.1/topics/i18n/
