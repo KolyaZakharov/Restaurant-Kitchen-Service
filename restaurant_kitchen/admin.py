16f406e750f9af8678a46a34cdffe53e0589bbb8
Mykola Zakharov <kolyazakharov94@gmail.com>
2023-01-29 23:48:58 +0200

    overloaded verbose_name_plural in Dish model and regisred admin models

--- restaurant_kitchen/admin.py
+++ restaurant_kitchen/admin.py
@@ -1,3 +1,46 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 
-# Register your models here.
+from restaurant_kitchen.models import DishType, Dish, Cook, Ingredient
+
+
+@admin.register(Ingredient)
+class IngredientAdmin(admin.ModelAdmin):
+    search_fields = ("name",)
+
+
+@admin.register(DishType)
+class DishTypeAdmin(admin.ModelAdmin):
+    search_fields = ("name",)
+
+
+@admin.register(Dish)
+class DishAdmin(admin.ModelAdmin):
+    list_display = ["name", "price", "dish_type",]
+    list_filter = ["price", "dish_type"]
+    search_fields = ("name",)
+
+
+@admin.register(Cook)
+class CookAdmin(UserAdmin):
+    list_display = UserAdmin.list_display + ("years_of_experience",)
+    fieldsets = UserAdmin.fieldsets + (
+        (("Additional info", {"fields": ("years_of_experience",)}),)
+    )
+    add_fieldsets = UserAdmin.add_fieldsets + (
+        (
+            (
+                "Additional info",
+                {
+                    "fields": (
+                        "first_name",
+                        "last_name",
+                        "years_of_experience",
+                    )
+                },
+            ),
+        )
+    )
+
+
+
--- restaurant_kitchen/models.py
+++ restaurant_kitchen/models.py
@@ -43,6 +43,7 @@ class Dish(models.Model):
 
     class Meta:
         ordering = ["name"]
+        verbose_name_plural = "dishes"
 
     def __str__(self):
         return f"{self.name} (price:{self.price}, dish type:{self.dish_type.name}"
