7c18bf544937efa0be2b070db745e6adb6ed48cf
Mykola Zakharov <kolyazakharov94@gmail.com>
2023-02-17 18:40:17 +0200

    added restaurant-kitchen-service-ei8j.onrender.com to ALLOWED-HOSTS

--- restaurant_kitchen_service/settings.py
+++ restaurant_kitchen_service/settings.py
@@ -32,7 +32,7 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!`
 DEBUG = os.environ.get("DJANGO_DEBUG", "") != "False"
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["restaurant-kitchen-service-ei8j.onrender.com", "127.0.0.1" ]
 
 INTERNAL_IPS = [
     "127.0.0.1",
